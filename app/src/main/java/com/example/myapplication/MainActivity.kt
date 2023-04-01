a9ba0198ee85c534cb7015bfd916a0314ef4bd78
qaedz <qaedzxsw@gmail.com>
2023-04-01 19:08:16 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val textView: TextView = findViewById(R.id.text_view)
+        val button1: Button = findViewById(R.id.button1)
+        val button2: Button = findViewById(R.id.button2)
+
+        button1.setOnClickListener {
+            textView.text = "學號:410715669"
+        }
+
+        button2.setOnClickListener {
+            textView.text = "姓名:賴威銡"
+        }
+
+
+    }
+}
