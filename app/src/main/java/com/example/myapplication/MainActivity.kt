a2fd27c116a969aee04fda75c561bf6078362599
qaedz <qaedzxsw@gmail.com>
2023-04-02 02:12:47 +0800

    1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.myapplication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +22,10 @@ class MainActivity : AppCompatActivity() {
         val textView: TextView = findViewById(R.id.text_view)
         val button1: Button = findViewById(R.id.button1)
         val button2: Button = findViewById(R.id.button2)
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        val adapter = ImageAdapter(this)
+        viewPager.adapter = adapter
+
 
         button1.setOnClickListener {
             textView.text = "學號:410715669"
@@ -25,6 +35,26 @@ class MainActivity : AppCompatActivity() {
             textView.text = "姓名:賴威銡"
         }
 
+    }
+}
 
+
+class ImageAdapter(private val context: Context) : PagerAdapter() {
+    private val images = arrayOf(R.drawable.an01, R.drawable.an02, R.drawable.an03)
+
+    override fun getCount(): Int = images.size
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val imageView = ImageView(context)
+        imageView.setImageResource(images[position])
+        container.addView(imageView)
+        return imageView
+    }
+
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean = view == `object`
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as View)
     }
 }
